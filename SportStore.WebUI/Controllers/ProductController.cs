8aa5b355a765fd2eccac21f9284b86455f49a9b3
Opilane TTHK <opilane@tthk.ee>
2020-09-23 11:45:44 +0300

    v1

---
+++ SportStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using SportStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SportStore.WebUI.Controllers
+{
+	public class ProductController : Controller
+	{
+		private IProductRepository repository;
+		public ProductController(IProductRepository productRepository)
+		{
+			repository = productRepository;
+		}
+		public ViewResult List(int page = 1)
+		{
+			return View(repository.Products
+			.OrderBy(p => p.ProductID)
+			.Skip((page - 1) * 200)
+			.Take(200));
+		}
+	}
+}
